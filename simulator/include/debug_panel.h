174a81c1d954f24c6187edac7c4823e639ed111a
moego0 <moego0@example.com>
2026-01-20 17:50:39 +0200

    Remove old project files

--- simulator/include/debug_panel.h
+++
@@ -1,9 +0,0 @@
-#ifndef DEBUG_PANEL_H
-#define DEBUG_PANEL_H
-
-// Debug panel rendering and event logging
-void debug_panel_init(void);
-void debug_panel_render(void);
-void debug_log_event(const char *format, ...);
-
-#endif // DEBUG_PANEL_H
